589d9142b8e64e8b589e08bc1647f124fdd594c9
𝙥𝙖𝙧𝙩𝙝𝙚𝙣𝙤𝙨 <138315505+nihilisticneuralnet@users.noreply.github.com>
2025-11-20 19:21:44 +0530

    Update AIDE.py

--- models/AIDE.py
+++ models/AIDE.py
@@ -293,6 +293,7 @@ class AIDE_Model(nn.Module):
         x_max1 = self.model_max(x_maxmax1)
     
         x_1 = (x_min + x_max + x_min1 + x_max1) / 4  # [b, 2048]
+        x_0 *= 0
     
         # default masks: keep both branches as in training/inference
         if branch_mask is None:
@@ -541,6 +542,7 @@ class AIDE_Model(nn.Module):
 def AIDE(resnet_path, convnext_path):
     model = AIDE_Model(resnet_path, convnext_path)
     return model
+
 
 
 
